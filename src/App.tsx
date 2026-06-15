4a9d36d73600a0ad2503792f1c988798e9291bb1
Gunther Sunum <guntergarcia761@gmail.com>
2026-06-14 22:29:39 -0600

    comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+
+// Root component, conecta todos los componentes y rutas de la aplicación
 import { AppRouter } from '@app/router'
 
+// Componente raíz — renderiza el sistema de rutas de la aplicación
 export function App() {
   return <AppRouter />
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import { App } from './App'
 
 import { AppProvider } from '@app/providers/app-provider'
 
+// Selecciona el contenedor en tu HTML y crea la raíz de React
+
 ReactDOM.createRoot(
   document.getElementById('root')!,
 ).render(
+  
   <React.StrictMode>
+   
     <AppProvider>
+      
       <App />
     </AppProvider>
   </React.StrictMode>,
